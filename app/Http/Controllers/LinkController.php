cf8f9387a51e311e3f3cf14dc66f55d4f595fc6b
gabrieladrianofx <gabrieladrianofx@gmail.com>
2025-05-24 00:27:40 -0300

    Relacionamentos

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -21,7 +21,10 @@ class LinkController extends Controller
      */
     public function store(StoreLinkRequest $request)
     {
-        Link::query()->create($request->validated());
+        /** @var User $user */
+        $user = auth()->user();
+
+        $user->links()->create($request->validated());
 
         return to_route('dashboard');
     }
