41847e61d1fb2c0c2b24e560d4f0e27d0453dca0
gabrieladrianofx <gabrieladrianofx@gmail.com>
2025-05-24 04:00:05 -0300

    Refatorando Ordenar os links

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -4,9 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Link extends Model
 {
     /** @use HasFactory<\Database\Factories\LinkFactory> */
     use HasFactory;
+
+    public function user()
+    {
+        return $this->BelongsTo(User::class);
+    }
+
+    public function moveUp()
+    {
+        $this->move(-1);
+    }
+
+    public function moveDown()
+    {
+        $this->move(+1);
+    }
+
+    /**
+     * Function responsible for reorder the link
+     *
+     * @param [int] $to + 1 for moving down and -1 for moving up
+     * @return void
+     */
+    private function move($to)
+    {
+        $order = $this->sort;
+        $newOrder = $order + $to;
+
+        $user = $this->user;
+
+        $swapWith = $user->links()->where('sort', '=', $newOrder)->first();
+
+        $this->fill(['sort' => $newOrder])->save();
+        $swapWith->fill(['sort' => $order])->save();
+
+    }
 }
